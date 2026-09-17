d0a7bbdf17f0a70435d43e81d34aa4871fda21c9
eywoo <wooyerine@gmail.com>
2026-09-17 22:11:27 +0900

    desktop-pet 2.1.2

--- Casks/desktop-pet.rb
+++ Casks/desktop-pet.rb
@@ -1,6 +1,6 @@
 cask "desktop-pet" do
-  version "2.1.1"
-  sha256 "697051025cd1cb5fda2ca9d8a71a04d9a5a1ce98e5e0a15496d91384756f5d35"
+  version "2.1.2"
+  sha256 "bafa8bc762e66e599cb61388d12b8de5153e2503e788b4a4d61ea825894fd9d1"
 
   url "https://github.com/wooyerine/desktop-pet/releases/download/v#{version}/desktop-pet-#{version}-arm64.dmg"
   name "Desktop Pet"
